d83acb57de4bcd1598f14f573e601b93fefb7fe3
HoodsDream <idinkyy@gmail.com>
2015-04-01 16:27:37 -0700

    first commit

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,18 +8,108 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var userCity: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+
+    @IBAction func findWeather(sender: AnyObject) {
+        
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + userCity.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if url != nil {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                var urlError = false
+                
+                var weather = ""
+                
+                if error == nil {
+                    
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                    
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlContentArray.count > 0 {
+                        
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                        
+                        weather = weatherArray[0] as String
+                        
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                    } else {
+                        
+                        urlError = true
+                        
+                    }
+                    
+                    
+                    
+                } else {
+                    
+                    urlError = true
+                    
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    self.userCity.resignFirstResponder()
+                    
+                    if urlError == true {
+                        
+                        self.showError()
+                        
+                    } else {
+                        
+                        self.resultLabel.text = weather
+                        
+                    }
+                }
+                
+            })
+            
+            task.resume()
+            
+            
+        } else {
+            
+            showError()
+            
+        }
+        
+        
+    }
+    
+    func showError() {
+        resultLabel.text = "Sorry could not find results for " + userCity.text + ". Please try again."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.userCity.delegate = self
+        
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Light)
+        self.blurView = UIVisualEffectView(effect:blurEffect)
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+
+
+
+
+
+
 
